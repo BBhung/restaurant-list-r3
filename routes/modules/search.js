f7e6e3157b8312e1a7344c37480ce006eca10623
BBhung <98695564+BBhung@users.noreply.github.com>
2022-06-12 23:08:36 +0800

    Modify search error

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const Restaurant = require('../../models/Restaurant') 
 
 //設定餐廳名字和類別搜尋找到特定的餐廳
-router.get('/search', (req, res) => {
+router.get('/', (req, res) => {
   const keywords = req.query.keywords
   Restaurant.find()
     .lean()
@@ -12,7 +12,8 @@ router.get('/search', (req, res) => {
       const filterRestaurantsData = restaurantsData.filter(
        restaurant => 
        restaurant.name.toLowerCase().includes(keywords.trim().toLowerCase()) ||
-       restaurant.category.includes(keywords.trim().toLowerCase())
+       restaurant.category.includes(keywords.trim().toLowerCase()) || 
+       restaurant.name_en.toLowerCase().includes(keywords.trim().toLowerCase())
       )
       res.render('index', {restaurantsData: filterRestaurantsData, keywords})
     })
